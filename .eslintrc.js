14a97667eaec94326b17c5b345f2636c8c8101e3
Matheus Vellone <matheusvellone@hotmail.com>
2017-10-17 22:41:22 -0200

    Eject and CSS Modules config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,20 @@
 module.exports = {
+    "ecmaFeatures": {
+      "jsx": true,
+      "modules": true
+    },
     "env": {
         "es6": true,
+        "node": true,
         "browser": true
     },
+    "parser": "babel-eslint",
     "parserOptions": {
         "ecmaVersion": 6,
         "sourceType": "module"
     },
-    "extends": "matheusvellone-base/frontend"
+    // "extends": "matheusvellone-base/frontend"
+    "plugins": [
+      "react"
+    ]
 };
---
+++ src/pages/Home/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from '../..//logo.svg';
+import styles from './styles.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className={styles.wrap}>
+        <header className={styles.header}>
+          <img src={logo} className={styles.logo} alt="logo" />
+          <h1 className={styles.title}>Welcome to React</h1>
+        </header>
+        <p className={styles.intro}>
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
