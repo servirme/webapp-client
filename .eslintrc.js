14084a68b913cee69c52f498b9dc1a4237e697cd
Matheus Vellone <matheusvellone@hotmail.com>
2017-10-17 22:17:31 -0200

    Base create react app + dotfiles

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "browser": true
+    },
+    "parserOptions": {
+        "ecmaVersion": 6,
+        "sourceType": "module"
+    },
+    "extends": "matheusvellone-base/frontend"
+};
